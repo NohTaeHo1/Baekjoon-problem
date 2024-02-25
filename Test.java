803188791a6b30038517d8b4e7f206e17bf596e2
NohTaeHo1 <skysapal@gmail.com>
2024-02-25 15:48:44 +0900

    240225 digit test

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double test = 1234.56789;
+        System.out.printf("%2.3f", test);
+    }
+}
