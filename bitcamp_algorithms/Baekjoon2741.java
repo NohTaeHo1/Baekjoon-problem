cb72ecc77995df27a829ea2c40003af4cf44a739
NohTaeHo1 <skysapal@gmail.com>
2024-04-28 16:46:13 +0900

    "."

---
+++ bitcamp_algorithms/Baekjoon2741.java
@@ -0,0 +1,13 @@
+package bitcamp_algorithms;
+
+import java.util.Scanner;
+
+public class Baekjoon2741 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        var input = sc.nextInt();
+        for(var i =0;i<input;i++){
+            System.out.println(i+1);
+        }
+    }
+}
